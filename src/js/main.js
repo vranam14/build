0e1ffaaaf64d512b46214058faf481b8a8bbe47f
Matěj Vrána <vranamatej@email.cz>
2022-08-09 14:59:26 +0200

    Modální okno v jQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,17 @@
-var button = document.querySelector('#button')
-var modal = document.querySelector('#modal')
-var close = document.querySelector('#close')
+$(document).ready(function(){
 
-button.addEventListener('click', function (){
-    modal.classList.add('modal_active');
-});
+    var button = $('#button');
+    var modal = $('#modal');
+    var close = $('#close');
 
-close.addEventListener('click', function (){
-    modal.classList.remove('modal_active');
-});
+    button.on('click', function(){
+        modal.addClass('modal_active');
+
+    })
+
+    close.on('click', function(){
+        modal.removeClass('modal_active');
+
+    })
+
+})
