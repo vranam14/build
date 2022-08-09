6123b64f5031042293db929e349cbafec4d180d4
Matěj Vrána <vranamatej@email.cz>
2022-08-09 17:08:01 +0200

    nákodování sekce portfolio, vytvoření slideru a responzivity

--- src/js/main.js
+++ src/js/main.js
@@ -14,4 +14,26 @@ $(document).ready(function(){
 
     })
 
+    $('.slider').slick({
+        slidesToShow: 3,
+        prevArrow: $('.arrows-left'),
+        nextArrow: $('.arrows-right'),
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+              slidesToShow: 2,
+             
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+              slidesToShow: 1,
+              }
+            },
+          
+        ]
+    });
+
 })
